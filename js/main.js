e6d600d8be7b237ccdacb5913763822aabd205f1
Rateb Ajamieh <ajamiehrateb@gmail.com>
2023-05-11 14:47:25 -0600

    Chess texture added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as CANNON from "cannon";
 
 //Chess Peice class
 class Box extends THREE.Mesh {
@@ -10,11 +11,9 @@ class Box extends THREE.Mesh {
     depth,
     color = "0x00ff00",
     velocity = { x: 0, y: 0, z: 0 },
+    material = new THREE.MeshStandardMaterial({ color: color }),
   }) {
-    super(
-      new THREE.BoxGeometry(width, height, depth),
-      new THREE.MeshStandardMaterial({ color: color })
-    );
+    super(new THREE.BoxGeometry(width, height, depth), material);
     this.height = height;
     this.width = width;
     this.depth = depth;
@@ -26,9 +25,15 @@ class Box extends THREE.Mesh {
   update(ground) {
     this.bottom = this.position.y - this.height / 2;
     this.top = this.position.y + this.height / 2;
-    this.position.y += this.velocity.y;
 
-    if (this.bottom <= ground.top) this.velocity.y = 0;
+    this.velocity.y += -0.005;
+
+    if (this.bottom + this.velocity.y <= ground.top) {
+      this.velocity.y = -this.velocity.y;
+      this.velocity.y *= 0.8;
+    } else {
+      this.position.y += this.velocity.y;
+    }
   }
 }
 
@@ -57,7 +62,7 @@ const cube = new Box({
     z: 0,
   },
 });
-cube.translateY(2);
+cube.translateY(5);
 cube.castShadow = true;
 scene.add(cube);
 
@@ -65,9 +70,20 @@ const plane = new Box({
   width: 10,
   height: 0.5,
   depth: 10,
-  color: 0xffff00,
+  color: 0xffffff,
+  material: [
+    new THREE.MeshLambertMaterial({ color: "lightgrey" }),
+    new THREE.MeshLambertMaterial({ color: "lightgrey" }),
+    new THREE.MeshStandardMaterial({
+      map: new THREE.TextureLoader().load("./img/Chess_Board.png"),
+    }),
+    new THREE.MeshLambertMaterial({ color: "lightgrey" }),
+    new THREE.MeshLambertMaterial({ color: "lightgrey" }),
+    new THREE.MeshLambertMaterial({ color: "lightgrey" }),
+  ],
 });
-plane.position.set(0, -2, 0);
+
+plane.position.set(0, 0, 0);
 plane.receiveShadow = true;
 scene.add(plane);
 camera.rotation.y = 1;
@@ -76,7 +92,7 @@ camera.position.set(5, 1, 1);
 //adding a light
 const light_color = 0xffffff;
 const light = new THREE.DirectionalLight(light_color, 1);
-light.position.set(0, 3, 2);
+light.position.set(0, 2, 0);
 light.castShadow = true;
 scene.add(light);
 
